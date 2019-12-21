86625ccf90f0351af715dfd3a2978e8d79f29ab9
Falchio <53504233+Falchio@users.noreply.github.com>
2019-12-22 00:02:52 +0300

    problem number 1 and 2 done

---
+++ src/Fruit.java
@@ -0,0 +1,5 @@
+public class Fruit {
+    public Fruit() {
+        float weight;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
-    public static void main(String[] args){
-        
+    public static <T> void main(String[] args) {
+        //problem number one
+        String[] stringArr = new String[]{"Первый", "Второй", "Третий", "Четвертый"};
+        Integer[] intArr = new Integer[]{1, 2, 3, 4};
 
+        changePlaces(stringArr);
+        changePlaces(intArr);
+
+        //problem number two
+        ArrayList<T> arrayListString = (ArrayList<T>) arrayToArrayList(stringArr);
+        ArrayList<T> arrayListInteger = (ArrayList<T>) arrayToArrayList(intArr);
+        System.out.println(arrayListString.get(0));
+        System.out.println(arrayListInteger.get(0));
+
+    }
+
+    public static void changePlaces(Object... args) {
+        System.out.println("Начальный массив: " + Arrays.toString(args));
+        Object firstElement;
+        firstElement = args[0];
+        for (int i = 0; i < args.length - 1; i++) {
+            args[i] = args[i + 1];
+        }
+        args[args.length - 1] = firstElement;
+        System.out.println("Смещенный массив: " + Arrays.toString(args));
+    }
+
+    public static <T> ArrayList<T> arrayToArrayList(T[]... args) {
+        return new ArrayList<T>(Arrays.<T>asList((T) args));
     }
 
 }
